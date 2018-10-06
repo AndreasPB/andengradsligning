928ba644cb6546c0b297e32eb468b4d2f2c355eb
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2018-10-06 14:56:22 +0200

    test data successful return a,b,c,d,x1,x2

--- andengradsligning.js
+++ andengradsligning.js
@@ -3,13 +3,26 @@
 
 andengradsligning(1,5,-6);
 
+
+
+
 function andengradsligning(a, b, c) {
     var x1;
     var x2;
-    var d;
+    var d = diskriminant(a, b, c)
+
+    x1 = (-b + Math.sqrt(d)) / 2*a;
+    x2 = (-b - Math.sqrt(d)) / 2*a;
+
+    console.log("x1 = ", x1, "x2 = ", x2)
+}
+
+
+
+function diskriminant(a, b, c) {
 
     d = Math.pow(b, 2) - 4 * a * c;
 
     console.log(d);
-
+    return d;
 }
