42eb8a42eb43ec83eb3fb40171673a1a88a716f2
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2018-10-06 14:43:08 +0200

    test added text variables

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
 // Opgave til CPHB - Andengradslining
+
+
+andengradsligning(1,5,-6);
+
+function andengradsligning(a, b, c) {
+    var x1;
+    var x2;
+    var d;
+
+    d = Math.pow(b, 2) - 4 * a * c;
+
+    console.log(d);
+
+}
